eeaddb45506157a27773fe5631a43b37b3c4b2a3
JaceDanish <47661433+JaceDanish@users.noreply.github.com>
2020-03-03 09:03:06 +0100

    Empty Console app

---
+++ ChessConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChessConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
