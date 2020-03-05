d9ee560685e1fc6d109371e69b9abbf6bce17b92
Philip Lind Jensen <jogjensen@hotmail.com>
2020-03-05 10:42:48 +0100

    Added  :IChessPiece

--- ChessClassLibrary/Model/King.cs
+++ ChessClassLibrary/Model/King.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ChessClassLibrary.Model;
 
 namespace ChessClassLibrary
 {
-    class King
+    class King : IChessPiece
     {
+
+
     }
 }
