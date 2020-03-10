75b17928408dbcb333db4c90f513ed9f20e8c7d6
Nikolai Køhler Lundberg Hansen <niko9946@edu.easj.dk>
2020-03-10 11:20:35 +0100

    Nikolaj: tilføjet reference

--- ChessConsoleApp/Program.cs
+++ ChessConsoleApp/Program.cs
@@ -6,6 +6,7 @@ namespace ChessConsoleApp
     {
         static void Main(string[] args)
         {
+            // Nikolaj kommentar for at kunne commite
             Console.WriteLine("Hello World!");
         }
     }
