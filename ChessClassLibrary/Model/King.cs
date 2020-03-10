bf1856b0454834a95d1fcfdb76d6f0de31a40fe8
Michael Faber Harder <mich719p@edu.easj.dk>
2020-03-10 09:08:32 +0100

    King modified

--- ChessClassLibrary/Model/King.cs
+++ ChessClassLibrary/Model/King.cs
@@ -25,32 +25,28 @@ namespace ChessClassLibrary
         public bool CheckMove(Game game, int x1, int y1, int x2, int y2)
         {
 
-            for (int i = y1 - 1; i < y1 + 2; i++)
+            for (int i = x1 - 1; i < x1 + 2; i++)
             {
-                for (int j = x1 - 1; j < x1 + 2; j++)
+                for (int j = y1 - 1; j < y1 + 2; j++)
                 {
-                    if (y2 == j && x2 == i)
+                    if (x2 == i && y2 == j)
                     {
-                        if (game.Board[j, i] != null)
+                        if (game.Board[i, j] != null)
                         {
-                            if (game.Board[j, i].IsWhite() == IsWhite())
+                            if (game.Board[i, j].IsWhite() == IsWhite())
                             {
                                 return false;
                             }
-                            else return true;
+                            return true;
                         }
-
                         else
                         {
                             return true;
                         }
                     }
                 }
-
             }
-            
             return false;
         }
-        
     }
 }
