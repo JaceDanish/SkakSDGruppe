88ec744f20f075a072daf1b4375a9bf6380346fb
JaceDanish <47661433+JaceDanish@users.noreply.github.com>
2020-03-17 09:05:37 +0100

    Knight implemented

--- ChessClassLibrary/Model/Knight.cs
+++ ChessClassLibrary/Model/Knight.cs
@@ -25,9 +25,15 @@ namespace ChessClassLibrary.Model
 
         public bool CheckMove(Game game, int x1, int y1, int x2, int y2)
         {
-            return true;
-            
-            //throw new NotImplementedException();
+            if (x1 == x2 || y1 == y2 || Math.Abs(Math.Abs(x1) - Math.Abs(x2)) > 2 || Math.Abs(Math.Abs(y1) - Math.Abs(y2)) > 2)
+                return false;
+
+            if (Math.Abs((Math.Abs(x1) + Math.Abs(y1)) - (Math.Abs(x2) + Math.Abs(y2))) == 3)
+                return true;
+            if (Math.Abs((Math.Abs(x1) + Math.Abs(y1)) - (Math.Abs(x2) + Math.Abs(y2))) == 1)
+                return true;
+
+            return false;
         }
 
 
