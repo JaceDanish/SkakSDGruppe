a7596a5c28c1f002e15d33ebf63d2cc562465c8b
JaceDanish <47661433+JaceDanish@users.noreply.github.com>
2020-03-22 12:35:18 +0100

    Porn

--- ChessClassLibrary/Model/Pawn.cs
+++ ChessClassLibrary/Model/Pawn.cs
@@ -8,19 +8,39 @@ namespace ChessClassLibrary.Model
 	class Pawn : IChessPiece
 	{
 		private bool _isWhite;
+        private bool _moved;
 
 		public Pawn(bool isWhite)
 		{
 			_isWhite = isWhite;
+            _moved = false;
 		}
 
 		public bool CheckMove(Game game, int x1, int y1, int x2, int y2)
-		{
-			
-			return false;
-		}
+        {
+            if (IsWhite() && x2 <= x1)
+                return false;
+            if (!IsWhite() && x1 <= x2)
+                return false;
+            if (_moved && (Math.Abs(x1 - x2) > 1 || Math.Abs(x2 - x1) > 1))
+                return false;
+            if (!_moved && (Math.Abs(x1 - x2) > 2 || Math.Abs(x2 - x1) > 2))
+                return false;
+            if (y1 != y2)
+            {
+                if (Math.Abs(y1 - y2) != 1 || Math.Abs(x1 - x2) != 1)
+                    return false;
+                if (game.Board[x2, y2] == null)
+                    return false;
+            }
+            else if (game.Board[x2, y2] != null)
+                return false;
 
-		public bool IsWhite()
+            _moved = true;
+            return true;
+        }
+
+        public bool IsWhite()
 		{
 			return _isWhite;
 		}
